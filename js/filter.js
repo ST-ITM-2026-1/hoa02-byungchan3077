03c5b8e469d80b6cf6a4729c33710e68e7c7de5a
byungchan3077 <byungchan3077@naver.com>
2026-04-14 01:36:55 +0900

    made filter.js and modified projects.html to fit to filter.js

---
+++ js/filter.js
@@ -0,0 +1,20 @@
+
+const filterBtns = document.querySelectorAll(".filter-btn");
+const projects = document.querySelectorAll(".projects-units li");
+
+filterBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        filterBtns.forEach(b => b.classList.remove("active"));
+        btn.classList.add("active");
+
+        const filter = btn.dataset.filter;
+
+        projects.forEach(project => {
+            if (filter === "all" || project.dataset.category === filter) {
+                project.style.display = "block";
+            } else {
+                project.style.display = "none";
+            }
+        });
+    });
+});
